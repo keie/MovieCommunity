9698001987d2abb561f36e8130446719869761c1
keie <hombrehealth@hotmail.com>
2019-08-28 15:08:35 -0400

    modal added

---
+++ MovieCommunity/Resources/js/popup.js
@@ -0,0 +1,9 @@
+﻿function closePop() {
+    document.getElementById('popUp').style.display = 'none';
+}
+
+function Test() {
+    //var str = "<div style='background-color: yellow;  height:90px; width:90px;'>'testing'</div>";
+    document.getElementById('popUp').style.display = 'block';
+}
+
