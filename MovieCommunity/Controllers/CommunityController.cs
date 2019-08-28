8c712ab8f35592ea9fda609b96dc4634866c0e8b
Kevin Nava <hombrehealth@hotmail.com>
2019-08-27 21:45:39 -0500

    local advance

---
+++ MovieCommunity/Controllers/CommunityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieCommunity.Controllers
+{
+    public class CommunityController : Controller
+    {
+        // GET: Community
+        public ActionResult Community()
+        {
+            return View();
+        }
+    }
+}
